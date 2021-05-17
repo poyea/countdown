edfdc15b5039d8e2512e1c1d337c73d60e41de6c
John Law <johnlaw.po@gmail.com>
2021-05-17 21:41:02 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <img alt="logo" src="./assets/logo.png" />
+  <Counter />
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+
+export default {
+  name: "App",
+  components: {
+    Counter,
+  },
+  created() {
+    document.title = "The Countdown";
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+html {
+  display: table;
+  margin: auto;
+}
+
+body {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  max-width: 100%;
+}
+</style>
---
+++ src/utils/time.js
@@ -0,0 +1,16 @@
+const msToFull = (ms) => {
+  let seconds = Math.floor(ms / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  hours = hours - days * 24;
+  minutes = minutes - days * 24 * 60 - hours * 60;
+  seconds = seconds - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+  return `${days} day${days > 1 ? "s" : ""} ${hours} hour${
+    hours > 1 ? "s" : ""
+  } ${minutes} minute${minutes > 1 ? "s" : ""} ${seconds} second${
+    seconds > 1 ? "s" : ""
+  }`;
+};
+export default msToFull;
